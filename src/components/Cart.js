e6e77a84a87f3eddf9e20fbf2c348868216939ea
Richad MERALLI <richad.meralli@gmail.com>
2021-07-23 18:04:39 +0200

    added some alerts

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/Cart.css'
 
 function Cart({ cart, updateCart }) {
   const monsteraPrice = 8
   const [isOpen, setIsOpen] = useState(true)
 
+  useEffect(()=>{
+      localStorage.setItem('cart', cart)
+      console.log(`cart a été modifié  ET SAUVEGARD et vaut mnt ${cart}`)
+  })
+
+  useEffect(() => {
+      console.log(`a ete sauvegardé ${localStorage.getItem('cart')}`)
+      
+  }, [])
+
   return isOpen ? (
       <div className='lmj-cart'>
           <button
               className='lmj-cart-toggle-button'
-              onClick={() => setIsOpen(false)}
-          >
+              onClick={() => setIsOpen(false)}>
               Fermer
           </button>
           <h2>Panier</h2>
